94ba9cb094bb3d66948a4ba463114d79a5b7315a
IPINST <ipi@ipinst.org>
2017-10-17 16:30:08 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -50,8 +50,8 @@ else:
     url = url_2
     
 ### Catch up on months missed. Replaces above lines of code. Be sure to delete the next three lines.
-url="http://www.un.org/en/peacekeeping/contributors/2017/jul17_3.pdf"
-prev_mon_abbr_1 = "jul"
+url="http://www.un.org/en/peacekeeping/contributors/2017/aug17_3.pdf"
+prev_mon_abbr_1 = "aug"
 year = 2017
 
 ### debugging. delete next  line after.
